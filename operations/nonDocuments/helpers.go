6e39bb3abeef844db7ec7ae0faebcb1d67a060dc
Zachary <leeyihong03@gmail.com>
2023-04-25 13:02:23 -0400

    Refactored operations/nonDocuments

---
+++ operations/nonDocuments/helpers.go
@@ -0,0 +1,123 @@
+package nondocuments
+
+import (
+	st "NoSequel/structures"
+	util "NoSequel/utils"
+	"bufio"
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+/*
+LineCounter counts the number of lines in an io.Reader.
+It reads the data from the reader in 32 KB chunks, counts the number of
+newline characters, and returns the total count of newlines (i.e. lines).
+If an error occurs while reading the data, the function returns the current
+count of lines and the error.
+*/
+func LineCounter(r io.Reader) (int, error) {
+	// Initialize a buffer to hold the data read from the reader.
+	buf := make([]byte, 32*1024)
+	// Initialize a counter to keep track of the number of lines.
+	count := 0
+	// Define a byte slice that represents the line separator (i.e. newline character).
+	lineSep := []byte{'\n'}
+	// Loop until the end of the reader is reached or an error occurs.
+	for {
+		// Read up to 32 KB of data from the reader into the buffer.
+		c, err := r.Read(buf)
+		// Increment the counter by the number of newline characters in the buffer.
+		count += bytes.Count(buf[:c], lineSep)
+		// If an error occurred while reading the data...
+		if err != nil {
+			// If the error was an EOF (i.e. end of file) error, return the final count of lines.
+			if err == io.EOF {
+				return count, nil
+			}
+			// Otherwise, return the current count of lines and the error.
+			return count, err
+		}
+	}
+}
+
+/*
+ReturnUidFromUsername reads the contents of the "admin-user" file and searches for the
+line that contains the specified username. If a line containing the username is found,
+the function returns the UID (user ID) associated with that username. If no matching line
+is found, the function returns an empty string.
+The "admin-user" file should have one line per user, with the format "username,uid".
+*/
+func ReturnUidFromUsername(username string) string {
+	// Open the "admin-user" file for reading.
+	readFile, err := os.Open(util.FindFolder("admin-user"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer readFile.Close()
+	// Create a new scanner to read the file line by line.
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	// Loop through each line in the file.
+	for fileScanner.Scan() {
+		// Split the line into separate fields based on the comma separator.
+		line := strings.SplitN(fileScanner.Text(), ",", 2)
+		// If the first field (i.e. the username) matches the specified username...
+		if line[0] == username {
+			// Return the UID (i.e. the second field) associated with the username.
+			return line[1]
+		}
+	}
+	// If no matching line was found, return an empty string.
+	return ""
+}
+
+/*
+ComparePassword reads the hashed password for the user with the specified UID from the "user" folder
+and compares it to the supplied password. If the passwords match, the function returns true. Otherwise,
+it returns false.
+*/
+func ComparePassword(uid string, suppliedPassword []byte) bool {
+	// Read the data for the user with the specified UID from the "user" folder.
+	data := util.ReadFile("user", uid, true)
+	// Unmarshal the data into a User struct.
+	user := st.User{}
+	st.Unmarshal(data, &user)
+	// Compare the hashed password for the user to the supplied password.
+	// Note: The user's password is stored as a byte slice that has been hashed using a secure hashing algorithm.
+	// To compare the passwords, we use the bytes.Equal function from the standard library.
+	return bytes.Equal(user.Password, suppliedPassword)
+}
+
+// GetAesKeyFromUsername retrieves the AES key for a given username.
+func GetAesKeyFromUsername(username string) []byte {
+	// Obtain the user ID associated with the provided username
+	uid := ReturnUidFromUsername(username)
+	// Read user data only once and unmarshal it
+	userData := st.User{}
+	userDataBytes := util.ReadFile("user", uid, true)
+	st.Unmarshal(userDataBytes, &userData)
+	return userData.AesKey
+}
+
+// GetPasswordFromUsername retrieves the password for a given username.
+func GetPasswordFromUsername(username string) []byte {
+	// Obtain the user ID associated with the provided username
+	uid := ReturnUidFromUsername(username)
+	// Read user data only once and unmarshal it
+	userData := st.User{}
+	userDataBytes := util.ReadFile("user", uid, true)
+	st.Unmarshal(userDataBytes, &userData)
+	return userData.Password
+}
+
+/*
+Takes a username and password. Returns true if the user's credentials match up, otherwise return false
+*/
+func CheckCredentials(username string, password []byte) bool {
+	aes := GetAesKeyFromUsername(username)
+	// Decrypt the password and check
+	return bytes.Equal(GetPasswordFromUsername(username), []byte(util.DecryptAES(aes, password)))
+}
--- operations/nonDocuments/user.go
+++ operations/nonDocuments/user.go
@@ -3,46 +3,10 @@ package nondocuments
 import (
 	st "NoSequel/structures"
 	util "NoSequel/utils"
-	"bufio"
-	"bytes"
-	"io"
 	"log"
 	"os"
 	"strconv"
-	"strings"
 )
-
-/*
-LineCounter counts the number of lines in an io.Reader.
-It reads the data from the reader in 32 KB chunks, counts the number of
-newline characters, and returns the total count of newlines (i.e. lines).
-If an error occurs while reading the data, the function returns the current
-count of lines and the error.
-*/
-func LineCounter(r io.Reader) (int, error) {
-	// Initialize a buffer to hold the data read from the reader.
-	buf := make([]byte, 32*1024)
-	// Initialize a counter to keep track of the number of lines.
-	count := 0
-	// Define a byte slice that represents the line separator (i.e. newline character).
-	lineSep := []byte{'\n'}
-	// Loop until the end of the reader is reached or an error occurs.
-	for {
-		// Read up to 32 KB of data from the reader into the buffer.
-		c, err := r.Read(buf)
-		// Increment the counter by the number of newline characters in the buffer.
-		count += bytes.Count(buf[:c], lineSep)
-		// If an error occurred while reading the data...
-		if err != nil {
-			// If the error was an EOF (i.e. end of file) error, return the final count of lines.
-			if err == io.EOF {
-				return count, nil
-			}
-			// Otherwise, return the current count of lines and the error.
-			return count, err
-		}
-	}
-}
 
 /*
 RegisterUser checks if the specified username already exists in the "admin-user" file. If the
@@ -80,54 +44,6 @@ func RegisterUser(username, password string) {
 	util.WriteJsonFile(st.Marshal(user), jsonFilePath)
 	// Append the user's username and UID to the "admin-user" file.
 	util.WriteTxtFile(username+","+nextUid+"\n", adminPath)
-}
-
-/*
-ReturnUidFromUsername reads the contents of the "admin-user" file and searches for the
-line that contains the specified username. If a line containing the username is found,
-the function returns the UID (user ID) associated with that username. If no matching line
-is found, the function returns an empty string.
-The "admin-user" file should have one line per user, with the format "username,uid".
-*/
-func ReturnUidFromUsername(username string) string {
-	// Open the "admin-user" file for reading.
-	readFile, err := os.Open(util.FindFolder("admin-user"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer readFile.Close()
-	// Create a new scanner to read the file line by line.
-	fileScanner := bufio.NewScanner(readFile)
-	fileScanner.Split(bufio.ScanLines)
-	// Loop through each line in the file.
-	for fileScanner.Scan() {
-		// Split the line into separate fields based on the comma separator.
-		line := strings.SplitN(fileScanner.Text(), ",", 2)
-		// If the first field (i.e. the username) matches the specified username...
-		if line[0] == username {
-			// Return the UID (i.e. the second field) associated with the username.
-			return line[1]
-		}
-	}
-	// If no matching line was found, return an empty string.
-	return ""
-}
-
-/*
-ComparePassword reads the hashed password for the user with the specified UID from the "user" folder
-and compares it to the supplied password. If the passwords match, the function returns true. Otherwise,
-it returns false.
-*/
-func ComparePassword(uid string, suppliedPassword []byte) bool {
-	// Read the data for the user with the specified UID from the "user" folder.
-	data := util.ReadFile("user", uid, true)
-	// Unmarshal the data into a User struct.
-	user := st.User{}
-	st.Unmarshal(data, &user)
-	// Compare the hashed password for the user to the supplied password.
-	// Note: The user's password is stored as a byte slice that has been hashed using a secure hashing algorithm.
-	// To compare the passwords, we use the bytes.Equal function from the standard library.
-	return bytes.Equal(user.Password, suppliedPassword)
 }
 
 /*
@@ -178,28 +94,6 @@ func SignIn(ciphertext []byte) []byte {
 	return util.EncryptRSA(plaintext.ClientPub, st.Marshal(ret))
 }
 
-// GetAesKeyFromUsername retrieves the AES key for a given username.
-func GetAesKeyFromUsername(username string) []byte {
-	// Obtain the user ID associated with the provided username
-	uid := ReturnUidFromUsername(username)
-	// Read user data only once and unmarshal it
-	userData := st.User{}
-	userDataBytes := util.ReadFile("user", uid, true)
-	st.Unmarshal(userDataBytes, &userData)
-	return userData.AesKey
-}
-
-// GetPasswordFromUsername retrieves the password for a given username.
-func GetPasswordFromUsername(username string) []byte {
-	// Obtain the user ID associated with the provided username
-	uid := ReturnUidFromUsername(username)
-	// Read user data only once and unmarshal it
-	userData := st.User{}
-	userDataBytes := util.ReadFile("user", uid, true)
-	st.Unmarshal(userDataBytes, &userData)
-	return userData.Password
-}
-
 // CreateIndexFile creates a new index file for the specified user and index.
 // It returns the file name of the created index file.
 func CreateIndexFile(uid, iid, username, indexname string) string {
@@ -228,8 +122,7 @@ func RegisterIndex(indexname, password []byte, username string) st.Response {
 	response := st.Response{}
 	// Get the AES key for the username
 	aes := GetAesKeyFromUsername(username)
-	// Decrypt the password and indexname using the AES key
-	decryptPassword := util.DecryptAES(aes, password)
+	// Decrypt indexname using the AES key
 	decryptIndexName := util.DecryptAES(aes, indexname)
 	// Get the user's unique ID
 	uid := ReturnUidFromUsername(username)
@@ -238,7 +131,7 @@ func RegisterIndex(indexname, password []byte, username string) st.Response {
 	userDataBytes := util.ReadFile("user", uid, true)
 	st.Unmarshal(userDataBytes, &userData)
 	// Check for password match before performing other operations
-	if !bytes.Equal(GetPasswordFromUsername(username), []byte(decryptPassword)) {
+	if !CheckCredentials(username, password) {
 		response.Message = []byte("Something went wrong. Might be a bad password")
 		response.Status = "403"
 		return response
