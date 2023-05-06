7fc4a05e54cdd446e7e3c49a07c68019dd5c5ae1
Zachary <leeyihong03@gmail.com>
2023-05-06 11:56:39 -0400

    added signIn handler

--- handlers.go
+++ handlers.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"github.com/labstack/echo"
-	"net/http"
-	st "NoSequel/structures"
 	nd "NoSequel/operations/nonDocuments"
+	st "NoSequel/structures"
+	"net/http"
+
+	"github.com/labstack/echo"
 )
 
 func deserializeInputJSON(c echo.Context) st.ServerReceive {
 	data := st.ServerReceive{}
 	if err := c.Bind(&data); err != nil {
 		data.DeserializeSuccesful = true
-	}else{
+	} else {
 		data.DeserializeSuccesful = false
 	}
 	return data
@@ -29,7 +30,17 @@ func register(c echo.Context) error {
 	resp := nd.RegisterUser(data.Username, data.PasswordString)
 	if resp.Status == "200" {
 		return c.JSON(http.StatusOK, resp)
-	}else{
+	} else {
+		return c.JSON(http.StatusBadRequest, resp)
+	}
+}
+
+func signIn(c echo.Context) error {
+	data := deserializeInputJSON(c)
+	resp := nd.SignIn(data.Payload)
+	if resp.Status == "200" {
+		return c.JSON(http.StatusOK, resp)
+	} else {
 		return c.JSON(http.StatusBadRequest, resp)
 	}
 }
